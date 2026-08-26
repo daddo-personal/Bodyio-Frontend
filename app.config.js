aacaa5ba9a20d93feaf9b4be31f7fe7cf9621762
daddo-personal <derrick.add0823@gmail.com>
2026-08-25 21:52:21 -0400

    Version 1.0.4

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default ({ config }) => ({
     name: "body.io",
     slug: "body-io",
     owner: "daddo0823",
-    version: "1.0.3",
+    version: "1.0.4",
     orientation: "portrait",
     icon: "./assets/images/BodyIO.png",
     scheme: "frontend",
